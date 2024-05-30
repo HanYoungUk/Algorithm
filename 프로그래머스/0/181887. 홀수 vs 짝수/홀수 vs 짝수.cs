c74112b245606389633fc0ce71d2f6743e3c5b02
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-05-30 12:50:42 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+       
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < num_list.Length; i = i + 2){
+            a += num_list[i];
+        }
+        for(int i = 1; i < num_list.Length; i = i + 2){
+            b += num_list[i];
+        }
+       
+        if( a >= b){
+            answer = a;
+        } else{
+            answer= b;
+        }
+        return answer;
+    }
+}
