0d588ae1122c30ae9fd7c1d8ea07261f000d15e5
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-05-30 13:07:02 +0900

    [level 0] Title: 정수 부분, Time: 0.12 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+       
+        answer = (int)flo;
+       
+       
+       
+        return answer;
+    }
+}
