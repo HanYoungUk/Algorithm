c6e4ffe73ee5e7e700b7e8de0945868a105859cb
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-06-06 21:57:05 +0900

    [level 0] Title: 0 떼기, Time: 0.76 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string n_str) {
+        string answer = "";
+        int x = Int32.Parse(n_str);
+        answer = x.ToString();
+       
+        return answer;
+    }
+}
