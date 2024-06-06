6d9917c0da39cf4f89c94ad04a228d80ff48d263
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-06-06 21:49:08 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+       
+         for(int i = 5; i >= 1; i = i - 2)
+         {
+              answer += hp / i;
+              hp = hp % i;
+              if (hp == 0)
+              {
+                  break;
+              }
+         }
+        return answer;
+    }
+}
