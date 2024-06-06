ca578117a6c79c7d4372aef425c720b99cd16cc6
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-06-06 21:55:18 +0900

    [level 0] Title: 카운트 다운, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
+        int count = 0;
+       
+        for(int i = start; i >= end_num; i--){
+            answer[count++] = i;
+        }
+       
+        return answer;
+    }
+}
