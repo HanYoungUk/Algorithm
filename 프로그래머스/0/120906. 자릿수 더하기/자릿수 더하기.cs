de68426052540947a12da56157f67efae33db679
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-06-06 21:45:03 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.36 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        string s = n.ToString();
+       
+       
+        for(int i = 0; i < s.Length; i++){
+            answer += s[i] - '0';
+
+        }
+       
+        return answer;
+    }
+}
