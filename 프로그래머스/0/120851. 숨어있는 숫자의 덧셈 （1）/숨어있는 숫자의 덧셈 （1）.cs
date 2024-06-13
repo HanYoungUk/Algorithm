60df0d8d84cf38a04e88cbe4226b76c31b6e7408
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-06-13 19:53:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.29 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string) {
+        int answer = 0;
+       
+        for(int i = 0; i < my_string.Length; i++)
+        {
+            if (my_string[i] >= 48 && my_string[i] <= 57)
+            {
+                answer += my_string[i] - '0';
+            }
+        }
+       
+        return answer;
+    }
+}
