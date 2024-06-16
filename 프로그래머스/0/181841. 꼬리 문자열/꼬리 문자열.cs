8ccc4fd3afb95fb0a4d6d0c58468c50254007dfb
HanYoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-06-16 15:57:49 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.26 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public string solution(string[] str_list, string ex) {
+        string answer = "";
+       
+        for(int i = 0; i < str_list.Length; i++){
+            if(str_list[i].Contains(ex)){
+                continue;
+            } else{
+                answer += str_list[i];
+            }
+        }
+        return answer;
+    }
+}
