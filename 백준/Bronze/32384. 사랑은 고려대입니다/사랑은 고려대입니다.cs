2f34ee42c1480e35473cb1d8958804f76a873e16
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2024-11-16 19:19:36 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int N = Int32.Parse(Console.ReadLine());
+
+                 for (int i = 0; i < N; i++)
+                {
+                 Console.Write("LoveisKoreaUniversity ");
+                }
+        }
+    }
+}
