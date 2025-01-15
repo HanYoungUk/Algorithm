dea4f276a026a288a871cbb0d7a66f253b2a0f93
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-01-16 06:45:52 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int apple = int.Parse(Console.ReadLine());
+            int mikan = int.Parse(Console.ReadLine());
+
+            int sum = 3 + apple + mikan;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
