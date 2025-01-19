32703738edc611ddf5569b2fb8487c87345ef541
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-01-19 14:48:53 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 48 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       _.-;;-._");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-;;-._|");
+            Console.WriteLine("'-..-'|   ||   |");
+            Console.WriteLine("'-..-'|_.-''-._|");
+        }
+    }
+}
