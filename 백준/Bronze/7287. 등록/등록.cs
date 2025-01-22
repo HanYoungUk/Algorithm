871587b119f4847dbbbb0e4dc097d7a019a6bde3
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-01-22 20:39:38 +0900

    [Bronze V] Title: 등록, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("278");
+            Console.WriteLine("gksdud9685");
+        }
+    }
+}
