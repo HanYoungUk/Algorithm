c3c9d3945af0339cea32093bfdaedd1b5a685470
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-01-22 20:38:56 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 52 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            string str = Console.ReadLine();
+
+            int result = 0;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
