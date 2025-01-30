f8428541e7b5670a352124304252518aeafa0963
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-01-30 14:29:04 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㄱ, Time: 60 ms, Memory: 5840 KB -BaekjoonHub

---
+++ 백준/Bronze/23802. 골뱅이 찍기 － 뒤집힌 ㄱ/골뱅이 찍기 － 뒤집힌 ㄱ.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             StringBuilder sb = new StringBuilder();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < 5 * count; j++)
+                {
+                    sb.Append("@");
+                }
+                sb.AppendLine();
+            }
+
+            for (int i = 0; i < count * 4; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append("@");
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
