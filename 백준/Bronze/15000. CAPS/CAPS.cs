bfbc7cc3db9c218668a5f148f0548a2d8d69e780
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-01-31 13:32:20 +0900

    [Bronze V] Title: CAPS, Time: 200 ms, Memory: 12444 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            str = str.ToUpper();
+
+            Console.WriteLine(str);
+        }
+    }
+}
