70fef1362f3b34fc8a9d451c3d6dfea918440753
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-02-12 17:02:48 +0900

    [Bronze V] Title: 時間 (Hour), Time: 48 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            int result = day * 24;
+
+            Console.WriteLine(result);
+        }
+    }
+}
