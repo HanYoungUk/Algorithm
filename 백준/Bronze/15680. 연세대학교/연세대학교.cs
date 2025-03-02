f3f8725133ea403f331f2e054757c38c3d7de230
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-03-02 19:42:47 +0900

    [Bronze V] Title: 연세대학교, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            if(count == 0)
+            {
+                Console.WriteLine("YONSEI");
+            }
+            else
+            {
+                Console.WriteLine("Leading the Way to the Future");
+            }
+        }
+    }
+}
