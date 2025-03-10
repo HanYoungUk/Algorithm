d9110ed964cf6d9ba2e5f8470ce6bee4a253ebbd
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-03-10 20:21:35 +0900

    [Bronze II] Title: 더하기, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] arr = str.Split(",");
+
+            long sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += int.Parse(arr[i]);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
