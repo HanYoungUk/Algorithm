a44ee9d34441f072583277bca65d44b8501ff70f
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-03-16 09:23:48 +0900

    [Bronze V] Title: AI 선도대학, Time: 52 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2024;
+
+            int count = int.Parse(Console.ReadLine());
+
+            int result = count - x;
+
+            Console.WriteLine(result);
+        }
+    }
+}
