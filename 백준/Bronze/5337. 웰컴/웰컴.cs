9f65a6eba20fddbfc00d65e12ae11bf641d42395
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-03-16 09:24:18 +0900

    [Bronze V] Title: 웰컴, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".  .   .");
+            Console.WriteLine("|  | _ | _. _ ._ _  _");
+            Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+        }
+    }
+}
