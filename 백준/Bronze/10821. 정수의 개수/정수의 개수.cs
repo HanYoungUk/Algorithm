226ce4e64a2500f345f64cca06316d0e0f22fe5c
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-04-10 06:58:52 +0900

    [Bronze II] Title: 정수의 개수, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] arr = str.Split(",");
+
+            Console.WriteLine(arr.Length);
+        }
+    }
+}
