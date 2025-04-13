98b6a7969bfaf16a01c2e9ff8c72c1437d704989
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-04-13 09:44:06 +0900

    [Bronze V] Title: 콜센터, Time: 40 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("     /~\\");
+            Console.WriteLine("    ( oo|");
+            Console.WriteLine("    _\\=/_");
+            Console.WriteLine("   /  _  \\");
+            Console.WriteLine("  //|/.\\|\\\\");
+            Console.WriteLine(" ||  \\ /  ||");
+            Console.WriteLine("============");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+        }
+    }
+}
