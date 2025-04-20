5a624d5bb7714b81cb4f9cab9963ccb1c979708c
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-04-20 09:49:15 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int v = int.Parse(Console.ReadLine());
+
+            int result = t * v;
+
+            Console.WriteLine(result);
+        }
+    }
+}
