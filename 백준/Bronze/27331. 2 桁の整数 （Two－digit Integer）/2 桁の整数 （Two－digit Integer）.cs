590e4a999ce2c0f5bbd9dadfdd0caefe5cb07bf1
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-05-01 18:57:43 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 52 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            string result = a + b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
