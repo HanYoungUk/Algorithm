9a0269efa531fb1d89d3381a9e550e77fafbe68e
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-05-11 15:45:36 +0900

    [Bronze V] Title: Java Warriors, Time: 52 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int result = count * 4000;
+
+            Console.WriteLine(result);
+        }
+    }
+}
