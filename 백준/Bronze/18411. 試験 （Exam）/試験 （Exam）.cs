6e2440501a1e32107c9266655dfc215319a9aafc
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-05-21 21:42:13 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 48 ms, Memory: 5608 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            var arr = str.Split(" ");
+            List<int> list = new List<int>();
+
+            foreach (var i in arr)
+            {
+                list.Add(int.Parse(i));
+            }
+
+            list.Sort();
+
+            int result = list[1] + list[2];
+
+            Console.WriteLine(result);
+        }
+    }
+}
