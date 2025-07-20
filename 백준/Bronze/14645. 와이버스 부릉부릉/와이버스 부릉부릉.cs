248953e687e00d5fe368bd725d7e25d63d290908
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-07-20 13:02:16 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string[] arr = str.Split(" ");
+
+            int count = int.Parse(arr[0]);
+
+            for (int i = 0; i < count; i++)
+            {
+                string str2 = Console.ReadLine();
+            }
+            Console.WriteLine("비와이");
+        }
+    }
+}
