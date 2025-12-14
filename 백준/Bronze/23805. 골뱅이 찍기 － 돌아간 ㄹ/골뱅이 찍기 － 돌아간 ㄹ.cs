898322272ea4df36340f5db688939f9dd80be62e
YoungUk <143507946+HanYoungUk@users.noreply.github.com>
2025-12-14 16:11:19 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 80 ms, Memory: 6668 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count * 3; j++)
+                {
+                    sb.Append("@");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append(" ");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append("@");
+                }
+                sb.AppendLine();
+            }
+
+            for (int i = 0; i < count * 3; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append("@");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append(" ");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append("@");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append(" ");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append("@");
+                }
+                sb.AppendLine();
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append("@");
+                }
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append(" ");
+                }
+                for (int j = 0; j < count * 3; j++)
+                {
+                    sb.Append("@");
+                }
+                sb.AppendLine();
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
